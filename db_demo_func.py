5c1c746f25c8444a8c333e8111785374149dd8e7
David Roesel <roesel@gmail.com>
2020-04-03 22:18:16 +0200

    Add delete_booking()

--- db_demo_func.py
+++ db_demo_func.py
@@ -13,3 +13,8 @@ def get_bookings_of_user(user_name)
 def get_bookings_of_room(room_name)
     room_bookings = Booking.select().join(Room).where(Room.name==room_name)
     return room_bookings
+    
+def delete_booking(id):
+    query = Booking.delete().where(Booking.id == id)
+    num_of_deleted_rows = query.execute()
+    return num_of_deleted_rows
