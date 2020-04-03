11b18af4ef7751b81ecf641b679e12871ee4b5fd
David Roesel <roesel@gmail.com>
2020-04-03 22:37:09 +0200

    converting get_bookings_* to list of dicts

--- db_demo_func.py
+++ db_demo_func.py
@@ -1,5 +1,6 @@
 import peewee
 from models import Room, Booking
+from playhouse.shortcuts import model_to_dict
 
 def create_booking(who, when, where):
     new_booking = Booking(who=who, where=where, when=when)
@@ -7,14 +8,24 @@ def create_booking(who, when, where):
     return new_booking.id
 
 def get_bookings_of_user(user_name):
-    user_bookings = Booking.select().where(Booking.who==user_name)
+    query = Booking.select().where(Booking.who==user_name)
+    user_bookings = [model_to_dict(c) for c in query]
     return user_bookings
 
 def get_bookings_of_room(room_name):
-    room_bookings = Booking.select().join(Room).where(Room.name==room_name)
+    query = Booking.select().join(Room).where(Room.name==room_name)
+    room_bookings = [model_to_dict(c) for c in query]
     return room_bookings
 
 def delete_booking(id):
     query = Booking.delete().where(Booking.id == id)
     num_of_deleted_rows = query.execute()
     return num_of_deleted_rows
+
+# --------- Debugging functions
+
+def print_bookings():
+    ''' Prints all bookings in terminal. '''
+    q = Booking.select() 
+    for b in q:
+        print(b.id, b.who, b.room.name)
