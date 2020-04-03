2c3a40c343a44aab24565f3b965cf2ce2c98027c
David Roesel <roesel@gmail.com>
2020-04-03 22:19:32 +0200

    Semicolons

--- db_demo_func.py
+++ db_demo_func.py
@@ -6,14 +6,14 @@ def create_booking(who, when, where):
     new_booking.save()
     return new_booking.id
 
-def get_bookings_of_user(user_name)
+def get_bookings_of_user(user_name):
     user_bookings = Booking.select().where(Booking.who==user_name)
     return user_bookings
 
-def get_bookings_of_room(room_name)
+def get_bookings_of_room(room_name):
     room_bookings = Booking.select().join(Room).where(Room.name==room_name)
     return room_bookings
-    
+
 def delete_booking(id):
     query = Booking.delete().where(Booking.id == id)
     num_of_deleted_rows = query.execute()
