0523f9cec8f435b81672e5154740e1731eb77742
SerhiiKulyk <sergiy.i.kulyk@gmail.com>
2020-04-05 18:38:27 +0200

    Update db_demo_func.py
    append fix

--- db_demo_func.py
+++ db_demo_func.py
@@ -140,19 +140,19 @@ def stats_for_plot_building(when):
 def stats_for_plot_time(building, month):
     for day in range(31):
         day_string = '2020-{:02d}-{:02d}'.format(month,day)
-        append.plot_input['labels'](day_string)
+        plot_input['labels'].append(day_string)
         building_occupation_AM, floor_occupation_AM, building_occupation_rel_AM, floor_occupation_rel_AM = stats_occupation(when = day_string + '-AM')
-        append.plot_input['data_AM'](building_occupation_AM[building])
+        plot_input['data_AM'].append(building_occupation_AM[building])
         if building_occupation_rel_AM[building] < 1:
-            append.plot_input['colors_AM']('007bff')
+            plot_input['colors_AM'].append('007bff')
         else:
-            append.plot_input['colors_AM']('dc3545')
+            plot_input['colors_AM'].append('dc3545')
         building_occupation_PM, floor_occupation_PM, building_occupation_rel_PM, floor_occupation_rel_PM = stats_occupation(when = day_string + '-PM')
-        append.plot_input['data_PM'](building_occupation_PM[building])
+        plot_input['data_PM'].append(building_occupation_PM[building])
         if building_occupation_rel_PM[building] < 1:
-            append.plot_input['colors_PM']('007bff')
+            plot_input['colors_PM'].append('007bff')
         else:
-            append.plot_input['colors_PM']('dc3545')
+            plot_input['colors_PM'].append('dc3545')
     return plot_input
 
 # --------- Debugging functions
